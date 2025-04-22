506cb3c182a60742843e2eb0ce51ed01c6f47f1a
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-22 12:09:14 -0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,43 +1,50 @@
+#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "shell.h"
+#include <string.h>
 
 /**
- * main - function
+ * main - Command interpreter function.
  *
- * Return: Always 0
+ * Return: 0 in succes
  */
 
 int main(void)
 {
-	char *line = NULL;
-	size_t bufsize = 0;
-	ssize_t bytes_read;
+        char *command = NULL;
+        size_t buffer_size = 0;
+        ssize_t char_readed;
+        int interact;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("#Simple_Shell$ ");
+        interact = isatty(STDIN_FILENO);
 
-		bytes_read = getline(&line, &bufsize, stdin);
-		if (bytes_read == -1)
-		{
-			if (isatty(STDIN_FILENO))
-			perror("Getline Error");
-			break;
-		}
+        while (1)
+        {
+                if (interact)
+                        write(STDOUT_FILENO, "$ ", 2);
 
-		line[strcspn(line, "\n")] = '\0';
-		if (strcmp(line, "exit") == 0)
-		{
-			if (isatty(STDIN_FILENO))
-				printf("Exit Shell...\n");
-			break;
-		}
-		execute_command(line);
-	}
-	free(line);
-	line = NULL;
-	return (0);
+                char_readed = getline(&command, &buffer_size, stdin);
+
+                if (char_readed == -1)
+                {
+                        if (interact)
+                                write(STDOUT_FILENO, "\n", 1);
+                        break;
+                }
+
+                if (char_readed > 0 && command[char_readed - 1] == '\n')
+                        command[char_readed - 1] = '\0';
+
+                if (strlen(command) == 0)
+                        continue;
+
+                if (strcmp(command, "exit") == 0)
+                        break;
+
+                exe(command);
+        }
+
+        free(command);
+        return (0);
 }
