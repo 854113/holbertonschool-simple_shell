a5a87afab8bbde92c344e79accaaee176725f25d
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-22 13:24:05 -0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,11 +2,15 @@
 #define SHELL_H
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-extern char **environ;
 void execute_command(char *command);
+char **parse_input(char *input);
+void free_array(char **array);
+int builtin_exit(char **args);
 
 #endif
