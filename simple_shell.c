e567640bb8c00a91bb28bb004b0a60d3361e2bb6
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-22 11:10:06 -0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -3,48 +3,67 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 /**
  * main - Command interpreter function.
  *
- * Return: 0 in succes
+ * Return: 0 on success
  */
-
 int main(void)
 {
-        char *command = NULL;
-        size_t buffer_size = 0;
-        ssize_t char_readed;
-        int interact;
+	char *command = NULL;
+	size_t buffer_size = 0;
+	ssize_t char_readed;
+	int interact;
+	pid_t pid;
 
-        interact = issaty(STDIN_FILENO);
+	interact = isatty(STDIN_FILENO);
 
-        while (1)
-        {
-                if (interact)
-                        write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		if (interact)
+			write(STDOUT_FILENO, "$ ", 2);
 
-                char_readed = getline(&command, &buffer_size, stdin);
+		char_readed = getline(&command, &buffer_size, stdin);
 
-                if (char_readed == -1)
-                {
-                        if (interact)
-                                write(STDOUT_FILENO, "\n", 1);
-                        break;
-                }
+		if (char_readed == -1)
+		{
+			if (interact)
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-                if (char_readed > 0 && command[char_readed - 1] == '\n')
-                        command[char_readed - 1] = '\0';
+		if (char_readed > 0 && command[char_readed - 1] == '\n')
+			command[char_readed - 1] = '\0';
 
-                if (strlen(command) == 0)
-                        continue;
+		if (strlen(command) == 0)
+			continue;
 
-                if (strcmp(command, "exit") == 0)
-                        break;
+		if (strcmp(command, "exit") == 0)
+			break;
 
-                exe(command)
-        }
+		pid = fork();
+		if (pid == 0)
+		{
+			char *argv[] = {command, NULL};
+			execve(command, argv, environ);
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
 
-        free(command);
-        return (0);
+	free(command);
+	return (0);
 }
