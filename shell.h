c62028ee8a3eaef2534861048f9db85bd55673cc
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-22 13:25:03 -0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_CMD_LEN 1024
+
+void prompt(void);
 void execute_command(char *command);
-char **parse_input(char *input);
-void free_array(char **array);
-int builtin_exit(char **args);
 
 #endif
