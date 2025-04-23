3b6c06353f648d73deb0e5275a68d7dcb95f1e8e
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-23 15:51:33 -0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,103 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include "shell.h"
 
 /**
- * execute_command - execute
- * @command: parameter
- * @path: path
- * Return: void
- */
-
-void exe_com(char *command, char *path)
-{
-	char *path_copy, *dir;
-	char full_path[1024];
-	int found = 0;
-
-	path_copy = strdup(path);
-	if (path_copy == NULL)
-	{
-		perror("PATH Error");
-		return;
-	}
-	dir = strtok(path_copy, ":");
-	while (dir != NULL)
-	{
-		snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-		if (access(full_path, X_OK) == 0)
-		{
-			found = 1;
-			pid_t pid = fork();
-
-			if (pid == 0)
-			{
-				char *args[] = {command, NULL};
-
-				execve(full_path, args, environ);
-				perror("Execve Error");
-				exit(EXIT_FAILURE);
-			}
-			else if (pid > 0)
-			{
-				wait(NULL);
-			}
-			else
-				perror("Fork Error");
-			break;
-		}
-		dir = strtok(NULL, ":");
-	}
-	free(path_copy);
-	if (!found)
-		printf("Command not found: %s\n", command);
-}
-
-/**
  * main - function
- * @argc: parameter
- * @argv: parameter
+ *
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	ssize_t bytes_read = 0;
-	size_t len = 0;
-	char *line = NULL, *path = getenv("PATH"), *token;
-
-	if (path == NULL)
-	{
-		perror("Failed to get PATH");
-		return (1);
-	}
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t bytes_read;
 
 	while (1)
 	{
-		printf("Simple_Shell$ ");
-		bytes_read = getline(&line, &len, stdin);
+		if (isatty(STDIN_FILENO))
+			printf("#Simple_Shell$ ");
+
+		bytes_read = getline(&line, &bufsize, stdin);
 		if (bytes_read == -1)
 		{
-			perror("Read Line Error");
+			if (isatty(STDIN_FILENO))
+			perror("Getline Error");
 			break;
 		}
 
-		token = strtok(line, " \n");
-		while (token != NULL)
+		line[strcspn(line, "\n")] = '\0';
+		if (strcmp(line, "exit") == 0)
 		{
-			if (strcmp(token, "exit") == 0)
-			{
-				free(line);
-				return (0);
-			}
-
-			exe_com(token, path);
-			token = strtok(NULL, " \n");
+			if (isatty(STDIN_FILENO))
+				printf("Exit Shell...\n");
+			break;
 		}
+		execute_command(line);
 	}
 	free(line);
+	line = NULL;
 	return (0);
 }
