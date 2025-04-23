f6ffbea9e0d789fd25f8ef2f17b3a12727aec59f
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-23 09:54:52 -0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -3,15 +3,17 @@
 #define PROMPT "#cisfun$ "
 
 /**
- * main - Simple UNIX command interpreter.
- * Return: Always 0.
+ * main - Simple UNIX command line interpreter
+ * Return: Always 0
  */
+
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t char_readed;
 	pid_t pid;
+	int status;
 
 	while (1)
 	{
@@ -20,12 +22,19 @@ int main(void)
 
 		char_readed = getline(&line, &len, stdin);
 		if (char_readed == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
 			break;
+		}
 
 		line[char_readed - 1] = '\0';
 
 		if (line[0] == '\0')
 			continue;
+
+		if (strcmp(line, "exit") == 0)
+			break;
 
 		pid = fork();
 		if (pid == 0)
@@ -35,14 +44,13 @@ int main(void)
 			argv[0] = line;
 			argv[1] = NULL;
 
-
 			execve(line, argv, environ);
-			perror("./hsh");
+			fprintf(stderr, "./hsh: %s: not found\n", line);
 			exit(1);
 		}
 		else if (pid > 0)
 		{
-			wait(NULL);
+			wait(&status);
 		}
 		else
 		{
@@ -51,5 +59,5 @@ int main(void)
 	}
 
 	free(line);
-		return (0);
+	return (0);
 }
