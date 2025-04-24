7095c46908482a7a63eaabed88d927b293c67e52
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-24 19:08:56 -0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,37 +1,34 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
+int last_exit_status = 0;
 
 /**
- * start_shell - Entry point of command interpreter.
- *
+ * start_shell - main loop of the shell
  * Return: 0
  */
+
 int start_shell(void)
 {
 	char *input_line = NULL;
 	size_t len = 0;
-	ssize_t chars_read;
+	ssize_t nread;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("$Simple_Shell ");
+			write(1, "$ ", 2);
 
-		chars_read = getline(&input_line, &len, stdin);
-		if (chars_read == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				printf("\n");
+		nread = getline(&input_line, &len, stdin);
+		if (nread == -1)
 			break;
-		}
 
-		input_line[strcspn(input_line, "\n")] = 0;
+		input_line[strcspn(input_line, "\n")] = '\0';
 
 		if (strcmp(input_line, "exit") == 0)
-			break;
+		{
+			free(input_line);
+			exit(last_exit_status);
+		}
 
 		run_com(input_line);
 	}
