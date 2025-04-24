5119ee46b99cf67786d642878b15b45020e7567e
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-24 18:59:00 -0300

    Update run_com.c

--- run_com.c
+++ run_com.c
@@ -1,168 +1,118 @@
-#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include "shell.h"
+
+extern char **environ;
 
 /**
- * trim_spaces - Trim leading and trailing spaces from a string
- * @str: The string to trim
+ * trim_spaces - remove leading and trailing spaces
+ * @str: input string
  *
- * Return: Pointer to the trimmed string
+ * Return: trimmed string
  */
 char *trim_spaces(char *str)
 {
-	char *end;
-
 	while (*str == ' ' || *str == '\t')
 		str++;
 
-	if (*str == 0)
-		return (str);
+	if (*str == '\0')
+		return str;
 
-	end = str + strlen(str) - 1;
+	char *end = str + strlen(str) - 1;
 	while (end > str && (*end == ' ' || *end == '\t'))
 		end--;
 
 	end[1] = '\0';
-
-	return (str);
+	return str;
 }
 
 /**
- * tokenize_input - Split input into tokens
- * @input_line: The input line to tokenize
- * @argv: Array to store tokens
- *
- * Return: Number of tokens or -1 if empty
- */
-int tokenize_input(char *input_line, char **argv)
-{
-	int i = 0;
-
-	argv[i] = strtok(input_line, " ");
-	if (argv[0] == NULL)
-		return (-1);
-
-	while (argv[i] != NULL && i < 63)
-		argv[++i] = strtok(NULL, " ");
-	argv[i] = NULL;
-
-	return (i);
-}
-
-/**
- * find_in_path - Find command in PATH
- * @command: Command to find
- * @environ: Environment variables
- *
- * Return: Full path to command or NULL if not found
- */
-char *find_in_path(char *command, char **environ)
-{
-	char *path_env = NULL, *path, *full_path;
-	int j;
-
-	for (j = 0; environ[j]; j++)
-	{
-		if (strncmp(environ[j], "PATH=", 5) == 0)
-		{
-			path_env = environ[j] + 5;
-			break;
-		}
-	}
-
-	if (!path_env || strchr(command, '/') != NULL)
-		return (NULL);
-
-	path = strtok(path_env, ":");
-	while (path)
-	{
-		full_path = malloc(strlen(path) + strlen(command) + 2);
-		if (!full_path)
-			return (NULL);
-
-		sprintf(full_path, "%s/%s", path, command);
-
-		if (access(full_path, X_OK) == 0)
-			return (full_path);
-
-		free(full_path);
-		path = strtok(NULL, ":");
-	}
-
-	return (NULL);
-}
-
-/**
- * execute_command - Execute the command with fork and execve
- * @command: Command to execute
- * @argv: Arguments for the command
- *
- * Return: Exit status of the command
- */
-int execute_command(char *command, char **argv)
-{
-	pid_t pid;
-	int status;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Fork Error");
-		return (1);
-	}
-
-	if (pid == 0)
-	{
-		if (execve(command, argv, environ) == -1)
-		{
-			perror(command);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(&status);
-		return (WEXITSTATUS(status));
-	}
-
-	return (0);
-}
-
-/**
- * run_com - Execute a command
- * @input_line: User input line
+ * run_com - execute a command
+ * @input_line: user input line
  *
  * Return: void
  */
 void run_com(char *input_line)
 {
 	char *argv[64];
-	char *cmd_path = NULL;
-	int tokens;
-	int replaced = 0;
+	char *path_env = NULL, *path, *full_path;
+	pid_t pid;
+	int i = 0, j, replaced = 0;
 
 	input_line = trim_spaces(input_line);
 	if (*input_line == '\0')
 		return;
 
-	tokens = tokenize_input(input_line, argv);
-	if (tokens == -1)
+	argv[i] = strtok(input_line, " ");
+	if (argv[0] == NULL)
 		return;
 
-	if (access(argv[0], X_OK) != 0)
+	while (argv[i] != NULL && i < 63)
+		argv[++i] = strtok(NULL, " ");
+
+	for (j = 0; environ[j]; j++)
 	{
-		cmd_path = find_in_path(argv[0], environ);
-		if (cmd_path)
+		if (strncmp(environ[j], "PATH=", 5) == 0)
 		{
-			argv[0] = cmd_path;
-			replaced = 1;
+			path_env = strdup(environ[j] + 5);
+			break;
 		}
 	}
 
-	execute_command(argv[0], argv);
+	if (strchr(argv[0], '/') == NULL && path_env)
+	{
+		path = strtok(path_env, ":");
+		while (path)
+		{
+			full_path = malloc(strlen(path) + strlen(argv[0]) + 2);
+			if (!full_path)
+			{
+				free(path_env);
+				return;
+			}
+
+			sprintf(full_path, "%s/%s", path, argv[0]);
+
+			if (access(full_path, X_OK) == 0)
+			{
+				argv[0] = full_path;
+				replaced = 1;
+				break;
+			}
+
+			free(full_path);
+			path = strtok(NULL, ":");
+		}
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Fork Error");
+		if (path_env)
+			free(path_env);
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
 
 	if (replaced)
-		free(cmd_path);
+		free(argv[0]);
+
+	if (path_env)
+		free(path_env);
 }
