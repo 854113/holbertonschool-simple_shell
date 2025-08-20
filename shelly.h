af3bec2c08b34358e6a41b7db0f3031bfb7ff006
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-08-20 15:30:00 -0300

    Update shelly.h

--- shelly.h
+++ shelly.h
@@ -1,19 +1,16 @@
-#ifndef SHELLY_H
-#define SHELLY_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
 extern char **environ;
 
-int print_prompt(void);
-void trim_line(char *s);
-char **build_argv(char *line);
-void free_argv(char **argv);
-int run_command(char *line, char *progname);
+void trim_line(char *line);
+int run_command(char *line, char *prog, unsigned int lineno);
 
 #endif
