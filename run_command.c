24f7583c0b4d33043743f8a5f247560e76beed3f
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-08-20 16:16:49 -0300

    Update run_command.c

--- run_command.c
+++ run_command.c
@@ -110,49 +110,34 @@ static char *find_cmd(char *cmd)
 
 int run_command(char *line, char *prog, unsigned int lineno)
 {
-	char **argv = make_argv(line);
-	char *cmd;
-	pid_t pid;
-	int status;
+    char **argv = make_argv(line);
+    char *cmd;
+    pid_t pid;
+    int status;
 
-	if (!argv || !argv[0])
-	{
-		free(argv);
-		return (0);
-	}
+    if (!argv || !argv[0]) { free(argv); return last_status; }
 
-	if (strcmp(argv[0], "exit") == 0)
-	{
-		free(argv);
-		free(line);
-		exit(status);
-	}
+    if (strcmp(argv[0], "exit") == 0)
+    {
+        free(argv);
+        free(line);
+        exit(last_status);
+    }
 
-	cmd = find_cmd(argv[0]);
-	if (!cmd)
-	{
-		fprintf(stderr, "%s: %u: %s: not found\n", prog, lineno, argv[0]);
-		free(argv);
-		return (127);
-	}
+    cmd = find_cmd(argv[0]);
+    if (!cmd)
+    {
+        fprintf(stderr, "%s: %u: %s: not found\n", prog, lineno, argv[0]);
+        free(argv);
+        return 127;
+    }
 
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(cmd, argv, environ);
-		perror(prog);
-		_exit(127);
-	}
-	if (pid < 0)
-	{
-		perror(prog);
-		free(cmd);
-		free(argv);
-		return (1);
-	}
+    pid = fork();
+    if (pid == 0) { execve(cmd, argv, environ); perror(prog); _exit(127); }
+    if (pid < 0) { perror(prog); free(cmd); free(argv); return 1; }
 
-	waitpid(pid, &status, 0);
-	free(cmd);
-	free(argv);
-	return (WIFEXITED(status) ? WEXITSTATUS(status) : 1);
+    waitpid(pid, &status, 0);
+    free(cmd);
+    free(argv);
+    return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
 }
