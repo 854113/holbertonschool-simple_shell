e6cb4caea605d9a17d122ac84035f126db91934f
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-08-21 10:04:29 -0300

    Update run_command.c

--- run_command.c
+++ run_command.c
@@ -79,20 +79,4 @@ int run_command(char *line, char *prog, unsigned int lineno)
         exit(0);
     }
 
-    cmd = find_cmd(argv[0]);
-    if (!cmd)
-    {
-        fprintf(stderr, "%s: %u: %s: not found\n", prog, lineno, argv[0]);
-        free(argv);
-        return 127;
-    }
-
-    pid = fork();
-    if (pid == 0) { execve(cmd, argv, environ); perror(prog); _exit(127); }
-    if (pid < 0) { perror(prog); free(cmd); free(argv); return 1; }
-
-    waitpid(pid, &status, 0);
-    free(cmd);
-    free(argv);
-    return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-}
+  
